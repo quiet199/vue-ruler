ed547aca10fbedac66d2d4bf90e233a4f52e0096
weitf <quiet191@163.com>
2022-05-10 19:33:18 +0800

    .

---
+++ src/views/CsRuler.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+                <Cruler @post-NumValue="rulerNum" 
+                    @scroll-start='startEvent' 
+                    @scroll-end="endEvent" 
+                    :NowNum='100' 
+                    :maxNum='200' 
+                    :minNum='-30' 
+                    :ruleWidth='40' 
+                    :numSize='50'></Cruler>
+    </div>
+</template>
+
+<script>
+    import Cruler from '@/components/Cruler.vue'
+    export default{
+        name:'CsRuler',
+        components: {
+            Cruler
+        },
+        data() {
+            return {
+                NumValue: '',
+                color: 'red'
+            }
+        },
+        methods: {
+            //子组件传递刻度表
+            rulerNum(vul){
+                this.NumValue = vul
+                // console.log(vul)
+            },
+            endEvent(val){
+                // console.log(val)
+            },
+            startEvent(val){
+                // console.log(val)
+            }
+        },
+    }
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="nav">
+        <router-link to="/">Home</router-link> | 
+        <router-link to="/csRuler">csRuler</router-link> 
+    </div>
+</template>
+
+<script>
+
+    export default{
+        name: "Home",
+        data() {
+            return {
+            }
+        },
+    }
+</script>
+
+<style lang="less" scoped>
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
